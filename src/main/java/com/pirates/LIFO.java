15d5871f5ffe1104da317272b191904bab3c7da0
john <jhnzvzd@gmail.com>
2016-02-02 23:55:42 +0200

    updated

--- src/main/java/com/pirates/LIFO.java
+++ src/main/java/com/pirates/LIFO.java
@@ -8,11 +8,15 @@ public class LIFO extends AbstractQueue {
     @Override
     public String pop() {
         String lastElement = null;
-        for (int i = getArr().length - 1; i >= 0; i--) {
-            if (getArr()[i] != null) {
-                lastElement = getArr()[i];
-                getArr()[i] = null;
-                break;
+        if (size() == 0) {
+            return "Очередь пуста";
+        } else {
+            for (int i = getArr().length - 1; i >= 0; i--) {
+                if (getArr()[i] != null) {
+                    lastElement = getArr()[i];
+                    getArr()[i] = null;
+                    break;
+                }
             }
         }
         return lastElement;
@@ -23,7 +27,7 @@ public class LIFO extends AbstractQueue {
     public String peek() {
         String lastElement = null;
         if (size() == 0) {
-            System.out.println("Add elements in the array");
+            return "Очередь пуста";
         } else {
             for (int i = getArr().length - 1; i >= 0; i--) {
                 if (getArr()[i] != null) {
@@ -38,7 +42,7 @@ public class LIFO extends AbstractQueue {
     /*Добавление элемента в массив и расширяет массив при переполнении*/
     @Override
     public void push(String element) {
-        if(!("").equals(element)) {
+        if (!("").equals(element)) {
             if (size() < getArr().length) {
                 for (int i = 0; i < getArr().length; i++) {
                     if (getArr()[i] == null) {
